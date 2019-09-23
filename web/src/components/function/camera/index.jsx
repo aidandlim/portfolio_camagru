c17bec130a023936ab1563bef0f176d1eb296ff6
Dongsub Lim <dlim@e1z2r5p45.42.us.org>
2019-09-22 22:46:21 -0700

    update camera

--- web/src/components/function/camera/index.jsx
+++ web/src/components/function/camera/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 
 import Webcam from "react-webcam";
-import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi';
+import { FiChevronDown, FiChevronsLeft, FiChevronsRight, FiCheck } from 'react-icons/fi';
 
 import './index.css';
 
@@ -17,8 +17,7 @@ function Camera() {
 	let images = [];
 
 	function capture() {
-		const imageBase64 = webcamRef.current.getScreenshot();
-		const image = `<div class='camera-gallery-image' style='background: url("${imageBase64}"); background-position: center; background-size: cover;'/>`;
+		const image = webcamRef.current.getScreenshot();
 		images[images.length] = image;
 		render();
 	}
@@ -40,8 +39,31 @@ function Camera() {
 		$('.camera-gallery-container').html("");
 		for(let i = images.length - 3 - index; i < images.length - index; i++) {
 			if(images[i] !== undefined)
-				$('.camera-gallery-container').html($('.camera-gallery-container').html() + images[i]);
+				$('.camera-gallery-container').html($('.camera-gallery-container').html() + `<div class='camera-gallery-image' style='background: url("${images[i]}"); background-position: center; background-size: cover;'/>`);
 		}
+	}
+
+	function preview(num) {
+		let number = 0;
+		if(images.length < 3) {
+			number = num;
+		} else {
+			number = images.length - 3 - index + num;
+		}
+		const preview = `<div class='camera-preview-image' style='background: url("${images[number]}"); background-position: center; background-size: cover;'/>`;
+		$('.camera-galley-check-icon').css('display', 'none');
+		$('.camera-preview-container').html(preview);
+		$('.camera-galley-check-icon').eq(num).css('display', 'block');
+		$('.camera-shoot').fadeOut(0);
+		$('.camera-preview-container').fadeIn(0);
+		$('.camera-rollback').fadeIn(0);
+	}
+
+	function rollback() {
+		$('.camera-galley-check-icon').css('display', 'none');
+		$('.camera-shoot').fadeIn(0);
+		$('.camera-preview-container').fadeOut(0);
+		$('.camera-rollback').fadeOut(0);
 	}
 
 	return (
@@ -59,10 +81,23 @@ function Camera() {
 				<div className='camera-loading'>
 					<div className='camera-loading-img'></div>
 				</div>
+				<div className='camera-preview-container'></div>
 				<div className='camera-shoot' onClick={ () => capture() }></div>
+				<FiChevronDown className='camera-rollback' onClick={ () => rollback() } />
 				<div className='camera-gallery'>
 					<div className='camera-gallery-arrow' onClick={ () => handleIndex('left') }><FiChevronsLeft className='camera-gallery-arrow-icon' /></div>
 					<div className='camera-gallery-container'></div>
+					<div className='camera-gallery-check'>
+						<div className='camera-gallery-check-zone' onClick={ () => preview(0) }>
+							<FiCheck className='camera-galley-check-icon' />
+						</div>
+						<div className='camera-gallery-check-zone' onClick={ () => preview(1) }>
+							<FiCheck className='camera-galley-check-icon' />
+						</div>
+						<div className='camera-gallery-check-zone' onClick={ () => preview(2) }>
+							<FiCheck className='camera-galley-check-icon' />
+						</div>
+					</div>
 					<div className='camera-gallery-arrow' onClick={ () => handleIndex('right') }><FiChevronsRight className='camera-gallery-arrow-icon' /></div>
 				</div>
 			</div>

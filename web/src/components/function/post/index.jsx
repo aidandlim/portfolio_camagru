e96703754a71117ab6d8c208f7addc81aed310e6
Aidan Lim <ortivo.sol@gmail.com>
2019-10-02 01:30:07 -0700

    update post

--- web/src/components/function/post/index.jsx
+++ web/src/components/function/post/index.jsx
@@ -13,7 +13,7 @@ function Post(props) {
 		<div className='post'>
 			<div className='post-profile'></div>
 			<div className='post-info-container'>
-				<div className='post-author'>{props.data.user_id}</div>
+				<div className='post-author'>{props.data.user_nickname}</div>
 				<div className='post-time'>3 HOURS AGO</div>
 				<div className='post-in'>In</div>
 				<div className='post-location'>{props.data.location}</div>
@@ -27,8 +27,8 @@ function Post(props) {
 				<div className='post-content'>
 					{props.data.content}
 				</div>
-				<div className='post-likes' onClick={ () => dispatch(content_id(props.id)) }>10 likes</div>
-				<div className='post-comments' onClick={ () => dispatch(content_id(props.id)) }>View all 6 comments</div>
+				<div className='post-likes' onClick={ () => dispatch(content_id(props.id)) }>{props.data.num_likes} likes</div>
+				<div className='post-comments' onClick={ () => dispatch(content_id(props.id)) }>View all {props.data.num_comments} comments</div>
 				<div className='post-comment-box' contentEditable='true' data-placeholder='Add a comment...'></div>
 				<div className='post-comment-post'>POST</div>
 			</div>

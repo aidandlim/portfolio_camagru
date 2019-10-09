f1692cddb80c4f32e2ed420160111b529590ea8d
Aidan Lim <ortivo.sol@gmail.com>
2019-10-09 12:38:41 -0700

    update server

--- server/src/main/java/com/aidandlim/camagru/dao/PostDao.java
+++ server/src/main/java/com/aidandlim/camagru/dao/PostDao.java
@@ -1,7 +1,6 @@
 package com.aidandlim.camagru.dao;
 
 import com.aidandlim.camagru.dto.Post;
-import com.aidandlim.camagru.dto.Token;
 import com.aidandlim.camagru.dto.User;
 import org.springframework.stereotype.Repository;
 

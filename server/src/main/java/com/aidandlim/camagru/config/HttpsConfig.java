2d79da2e12d3caa484a38e3a6adfeab9e7986deb
Aidan Lim <ortivo.sol@gmail.com>
2019-10-17 00:00:34 -0700

    update build

--- server/src/main/java/com/aidandlim/camagru/config/HttpsConfig.java
+++ server/src/main/java/com/aidandlim/camagru/config/HttpsConfig.java
@@ -12,35 +12,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HttpsConfig {
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    @Value("${server.port.http}")
-            int httpPort;
-
-    @Value("${server.port}")
-            int httpsPort;
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-        return connector;
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//        return tomcat;
+//    }
+//
+//    @Value("${server.port.http}")
+//            int httpPort;
+//
+//    @Value("${server.port}")
+//            int httpsPort;
+//
+//    private Connector redirectConnector() {
+//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        connector.setPort(httpPort);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(httpsPort);
+//        return connector;
+//    }
 }

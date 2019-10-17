d7bcdf50e5e5f8c32dc8a192bd351cb888458739
Aidan Lim <ortivo.sol@gmail.com>
2019-10-17 03:01:03 -0700

    update build

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -2,8 +2,8 @@ package com.aidandlim.camagru.config;
 
 public class Const {
 
-    public static String URL_SERVER = "https://camagru.aidandlim.com:8443/";
-    public static String URL_APP = "https://camagru.aidandlim.com:3000/";
+    public static String URL_SERVER = "https://52.53.219.65:8443/";
+    public static String URL_APP = "https://52.53.219.65:3000/";
 
     public static String PATH_PICTURE = "/Users/aidan/Workspace/portfolio_camagru/data/picture/";
     public static String PATH_STICKER = "/Users/aidan/Workspace/portfolio_camagru/data/sticker/";

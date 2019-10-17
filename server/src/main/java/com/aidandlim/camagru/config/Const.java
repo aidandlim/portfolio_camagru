c99dbee0b69d2dca5855db4c13d49401b48670d6
Aidan Lim <ortivo.sol@gmail.com>
2019-10-17 03:06:24 -0700

    build test

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -5,6 +5,6 @@ public class Const {
     public static String URL_SERVER = "https://52.53.219.65:8443/";
     public static String URL_APP = "https://52.53.219.65:3000/";
 
-    public static String PATH_PICTURE = "/Users/aidan/Workspace/portfolio_camagru/data/picture/";
-    public static String PATH_STICKER = "/Users/aidan/Workspace/portfolio_camagru/data/sticker/";
+    public static String PATH_PICTURE = "/home/ubuntu/camagru/data/picture";
+    public static String PATH_STICKER = "/home/ubuntu/camagru/data/sticker/";
 }

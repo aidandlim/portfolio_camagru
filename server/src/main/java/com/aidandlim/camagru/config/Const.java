e9317493b6947146c9219d2b1d77f32929023716
Aidan Lim <ortivo.sol@gmail.com>
2019-10-16 23:16:18 -0700

    update URL

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -2,8 +2,8 @@ package com.aidandlim.camagru.config;
 
 public class Const {
 
-    public static String URL_SERVER = "https://localhost:8443/";
-    public static String URL_APP = "https://localhost:3000/";
+    public static String URL_SERVER = "https://54.153.127.241:8443/";
+    public static String URL_APP = "https://54.153.127.241:3000/";
 
     public static String PATH_PICTURE = "/Users/aidan/Workspace/portfolio_camagru/data/picture/";
     public static String PATH_STICKER = "/Users/aidan/Workspace/portfolio_camagru/data/sticker/";

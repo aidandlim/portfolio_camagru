0ee91d0e16df2603fdeceeb11b775bf8df011904
Aidan Lim <ortivo.sol@gmail.com>
2019-10-17 00:20:06 -0700

    update build

--- server/src/main/java/com/aidandlim/camagru/CamagruApplication.java
+++ server/src/main/java/com/aidandlim/camagru/CamagruApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CamagruApplication {
 
-    static String URL = "https://10.10.136.78:8443/";
-
     public static void main(String[] args) {
         SpringApplication.run(CamagruApplication.class, args);
     }

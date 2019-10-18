fbbd15f1d3e2396de48b89e5fc0df19220290ed2
Aidan Lim <ortivo.sol@gmail.com>
2019-10-17 21:09:02 -0700

    update build

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -3,23 +3,23 @@ package com.aidandlim.camagru.config;
 public class Const {
 
     /* AWS */
-    /*
-    public static String URL_SERVER = "https://camagru.aidandlim.com:8443/";
-    public static String URL_APP = "https://camagru.aidandlim.com:3000/";
+    /* */
+    public static String URL_SERVER = "https://52.53.219.65:8443/";
+    public static String URL_APP = "https://52.53.219.65:3000/";
 
     public static String PATH_PICTURE = "/home/ubuntu/camagru/data/picture/";
     public static String PATH_STICKER = "/home/ubuntu/camagru/data/sticker/";
-    */
+    /* */
 
     /* LOCAL */
-    /* */
+    /*
     public static String URL_SERVER = "https://localhost:8443/";
     public static String URL_APP = "https://localhost:3000/";
 
     public static String PATH_PICTURE = "/Users/aidan/Workspace/portfolio_camagru/data/picture/";
     public static String PATH_STICKER = "/Users/aidan/Workspace/portfolio_camagru/data/sticker/";
     public static String PATH_STATIC = "/Users/aidan/Workspace/portfolio_camagru/data/static/";
-    /* */
+    */
 
     /* TEST */
     /*

ed39dbab33ce5ff41d532af841580a3c861858d9
Aidan Lim <ortivo.sol@gmail.com>
2019-10-18 21:55:28 -0700

    update deployment setting

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -1,10 +1,21 @@
 package com.aidandlim.camagru.config;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Paths;
 
 public class Const {
 
     static String absolutePath = Paths.get("").toAbsolutePath().toString() + "/src/main/resources";
+    static String ip;
+
+    static {
+        try {
+            ip = InetAddress.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+
+        }
+    }
 
     /* AWS */
     /*
@@ -26,8 +37,8 @@ public class Const {
 
     /* 42 */
     /* */
-    public static String URL_SERVER = "https://localhost:8443/";
-    public static String URL_APP = "https://localhost:3000/";
+    public static String URL_SERVER = "https://" + ip + ":8443/";
+    public static String URL_APP = "https://" + ip + ":3000/";
 
     public static String PATH_PICTURE = absolutePath + "/data/picture/";
     public static String PATH_STICKER = absolutePath + "/data/sticker/";

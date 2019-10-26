69926df0528d6c4d72c86d9dcb9fd4f8880c6568
Aidan Lim <ortivo.sol@gmail.com>
2019-10-25 17:56:10 -0700

    service deployment

--- server/src/main/java/com/aidandlim/camagru/config/Const.java
+++ server/src/main/java/com/aidandlim/camagru/config/Const.java
@@ -7,21 +7,21 @@ public class Const {
     static String absolutePath = Paths.get("").toAbsolutePath().toString() + "/src/main/resources";
 
     /* SERVICE */
-    /*
+    /* */
     public static String URL_SERVER = "https://camagru.aidandlim.com:8443/";
     public static String URL_APP = "https://camagru.aidandlim.com/";
 
     public static String PATH_PICTURE = absolutePath + "/data/picture/";
     public static String PATH_STICKER = absolutePath + "/data/sticker/";
-    */
+    /* */
 
     /* LOCAL */
-    /* */
+    /*
     public static String URL_SERVER = "https://localhost:8443/";
     public static String URL_APP = "https://localhost:3000/";
 
     public static String PATH_PICTURE = absolutePath + "/data/picture/";
     public static String PATH_STICKER = absolutePath + "/data/sticker/";
-    /* */
+    */
 
 }
